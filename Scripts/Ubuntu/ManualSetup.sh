e1bca60fee3ec55c0bb8b13e72d57ec9ce5e036d
Robinson <robinson.besson@nosseb.fr>
2020-05-08 15:18:17 +0200

    Log for manual setup

--- Scripts/Ubuntu/ManualSetup.sh
+++ Scripts/Ubuntu/ManualSetup.sh
@@ -8,16 +8,25 @@ if [ -z "$1" ]
 fi
 
 # REQUIREMENTS
+printf "\n\n\nInstalling requirements\n=======================\n\n"
 sudo apt-get install steamcmd
+printf "\n\nPath export\n===========\n"
 export PATH=/usr/games:$PATH
 
 
 # EBS PERSISTENT STORAGE
 
 # mount
+printf "\n\nMounting persistent storage\n===========================\n\n"
+printf "#sudo nvme list\n"
+sudo nvme list
 ebsArma=$(sudo nvme list | grep $1 | cut -d " " -f1) # path to ebs storage
+printf "\n\n#ebsArma : $ebsArma\n\n"
+printf "mount\n"
 sudo mount $ebsArma /home/steam/backup # mount
+printf "\nChange owner\n"
 sudo chown steam:steam /home/steam/backup # change owner
 
 # links
+printf "\nCreate Link\n"
 sudo -u steam ln -s /home/steam/backup/config /home/steam/config
