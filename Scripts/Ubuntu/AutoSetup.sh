7be58312ec8b4cf0ea132111d0ab6866785c025a
Robinson <robinson.besson@nosseb.fr>
2020-05-08 10:43:31 +0200

    Update version number

--- Scripts/Ubuntu/AutoSetup.sh
+++ Scripts/Ubuntu/AutoSetup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version="0.2"
+version="0.3"
 
 
 # ENABLE LOGGING
--- Scripts/Ubuntu/LaunchTemplate.sh
+++ Scripts/Ubuntu/LaunchTemplate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version="1.0"
+version="1.1"
 curl https://raw.githubusercontent.com/nosseb/ArmaServer/master/Scripts/ubuntu/AutoSetup.sh --output /home/ubuntu/AutoSetup.sh
 chmod +x /home/ubuntu/AutoSetup.sh
 sudo /home/ubuntu/AutoSetup.sh
