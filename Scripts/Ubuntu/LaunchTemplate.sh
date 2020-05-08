98ac28296ca29f26e37cc00b962b067b06b80d05
Robinson <robinson.besson@nosseb.fr>
2020-05-08 15:10:55 +0200

    Added marker for completed auto setup.

--- Scripts/Ubuntu/LaunchTemplate.sh
+++ Scripts/Ubuntu/LaunchTemplate.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-version="1.2"
+version="1.4"
 
 # Download initial script
 curl https://raw.githubusercontent.com/nosseb/ArmaServer/master/Scripts/Ubuntu/AutoSetup.sh --output /home/ubuntu/AutoSetup.sh
 chmod +x /home/ubuntu/AutoSetup.sh # add execution rights
 
 # Run
-sudo /home/ubuntu/AutoSetup.sh
+sudo bash /home/ubuntu/AutoSetup.sh
+
+touch /home/ubuntu/.setup_complete
