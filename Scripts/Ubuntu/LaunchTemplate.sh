f41eb44cd4047f3cf2a1a7ae0ff764f304e9615f
Robinson <robinson.besson@nosseb.fr>
2020-05-08 10:08:48 +0200

    Fixed links and directory issues. Links to download files were invalid (placeholders). Files wher put at root, and not /home/Ubuntu/ .

--- Scripts/Ubuntu/LaunchTemplate.sh
+++ Scripts/Ubuntu/LaunchTemplate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 version="1.0"
-curl https://LinkToAutoSetup.sh --output AutoSetup.sh
-chmod +x ./AutoSetup.sh
-sudo ./AutoSetup.sh
+curl https://raw.githubusercontent.com/nosseb/ArmaServer/master/Scripts/Ubuntu/AutoSetup.sh --output /home/Ubuntu/AutoSetup.sh
+chmod +x /home/Ubuntu/AutoSetup.sh
+sudo /home/Ubuntu/AutoSetup.sh
