a472e81c3d1296737f33483fe0b5a3e21b0cd5b1
Robinson <robinson.besson@nosseb.fr>
2020-05-08 13:02:08 +0200

    Improved log, comments.
    Added log messages, as well as a handfull of comments.

--- Scripts/Ubuntu/AutoSetup.sh
+++ Scripts/Ubuntu/AutoSetup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 version="0.4"
 
 
@@ -9,11 +10,11 @@ trap 'exec 2>&4 1>&3' 0 1 2 3 RETURN # Restore file descriptors.
 # The RETURN pseudo sigspec restore file descriptors each time a shell function or a script executed with the . or source builtins finishes executing.
 exec 1>/home/ubuntu/log.out 2>&1
 
-echo "AutoSetup.sh version" $version
+printf "AutoSetup.sh version $version\n"
 
 
 # REQUIREMENTS
-
+printf "\n\n\n\nInstalling requirements\n=======================\n\n"
 sudo add-apt-repository -y ppa:sbates # Repository for nvme-cli
 sudo add-apt-repository -y multiverse # Repository for SteamCMD
 sudo dpkg --add-architecture i386
@@ -23,17 +24,21 @@ sudo apt-get -y install lib32gcc1 # SteamCMD requirement
 
 
 # USER CREATION
-
+printf "\n\n\n\nAdding user\n"
 sudo useradd -m -s /bin/bash steam
 sudo -u steam mkdir /home/steam/local
 sudo -u steam mkdir /home/steam/backup
-
+ls -a /home/steam
 
 # LOCAL INSTACE STORAGE
-
+printf "\n\n\n\nMounting local storage\n\n"
+printf "#sudo nvme list\n"
+sudo nvme list
+printf "\n\n"
 InstanceDevice=$(sudo nvme list | grep Instance | cut -d " " -f1) # path to local storage device
 End="p1"
 InstancePart=$InstanceDevice$End # path to local storage partition
+printf "\n\nInstance partition: $InstancePart\n\n"
 
 # create partition
 sudo sfdisk $InstanceDevice << EOF
@@ -45,6 +50,9 @@ sudo mkfs.ext4 $InstancePart # format partition
 # mount
 sudo mount $InstancePart /home/steam/local # mount partition
 sudo chown -R steam:steam /home/steam/local # change owner
+printf "\n\n#lsblk\n"
+lsblk
+printf "\n\n"
 
 # creating .steam folder
 # not needed on reboot
@@ -62,11 +70,18 @@ then
 fi
 sudo -u steam ln -s /home/steam/local/Steam /home/steam/Steam
 
+printf "\n\n#ls -lha /home/steam/ \n"
+ls -lha /home/steam/
+printf "\n\n#ls -lha /home/steam/local/\n"
+ls -lha /home/steam/local/
+
 
 # DOWNLOAD ADDITIONAL SCRIPTS
-
+printf "\n\n\n\nDownloading additional files\n\n"
 curl https://raw.githubusercontent.com/nosseb/ArmaServer/master/Scripts/Ubuntu/ManualSetup.sh --output /home/ubuntu/ManualSetup.sh
 chmod +x /home/ubuntu/ManualSetup.sh
+printf "#ls -lha /home/ubuntu"
+ls -lha /home/ubuntu/
 
 
 # QUIT AND DESABLE LOGGING
