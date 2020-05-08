3be9fbdddec0c9a713a7cbc9e95fd129d1c6ab23
Robinson <robinson.besson@nosseb.fr>
2020-05-08 11:07:29 +0200

    Take volume ID as parameter
    Return error if no parameter.

--- Scripts/Ubuntu/ManualSetup.sh
+++ Scripts/Ubuntu/ManualSetup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 version='0.2'
 
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+    exit 128
+fi
+
 # REQUIREMENTS
 sudo apt-get install steamcmd
 export PATH=/usr/games:$PATH
@@ -9,7 +15,7 @@ export PATH=/usr/games:$PATH
 # EBS PERSISTENT STORAGE
 
 # mount
-ebsArma=$(sudo nvme list | grep VolumeID | cut -d " " -f1) # path to ebs storage
+ebsArma=$(sudo nvme list | grep $1 | cut -d " " -f1) # path to ebs storage
 sudo mount $ebsArma /home/steam/backup # mount
 sudo chown steam:steam /home/steam/backup # change owner
 
