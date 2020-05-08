e21b0d8f5a5ec9f1c7d73d95e6b693bd3389c738
Robinson <robinson.besson@nosseb.fr>
2020-05-08 13:02:08 +0200

    Improved log, comments.
    Added log messages, as well as a handfull of comments.

--- Scripts/Ubuntu/LaunchTemplate.sh
+++ Scripts/Ubuntu/LaunchTemplate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
 version="1.2"
+
+# Download initial script
 curl https://raw.githubusercontent.com/nosseb/ArmaServer/master/Scripts/Ubuntu/AutoSetup.sh --output /home/ubuntu/AutoSetup.sh
-chmod +x /home/ubuntu/AutoSetup.sh
+chmod +x /home/ubuntu/AutoSetup.sh # add execution rights
+
+# Run
 sudo /home/ubuntu/AutoSetup.sh
