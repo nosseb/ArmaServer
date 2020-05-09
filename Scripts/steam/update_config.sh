655815f047ea22512d9db33d26724d161b0c31a8
Robinson <robinson.besson@nosseb.fr>
2020-05-09 18:39:13 +0200

    Fix: .config.txt not being removed

--- Scripts/steam/update_config.sh
+++ Scripts/steam/update_config.sh
@@ -2,19 +2,19 @@
 
 cp -p ~/backup/config/*.cfg ~/local/arma3/
 
-if test -f "~/password.txt"
+if [ -f ~/password.txt ]
 then
     ls ~/backup/config/*.cfg > ~/.config.txt
-    local config="~/.config.txt"
+    config="~/.config.txt"
     while IFS= read -r file
     do
-        local passwords="~/password.txt"
+        passwords="~/password.txt"
         while IFS= read -r password
         do
-            local old=$(echo $password | cut -d" " -f1)
-            local new=$(echo $password | cut -d" " -f2)
+            old=$(echo $password | cut -d" " -f1)
+            new=$(echo $password | cut -d" " -f2)
             sed -i "s/${old}/${new}/g" $file
         done < "$passwords"
     done < "$config"
-    rm ~/.config.txt
+    rm .config.txt
 fi
